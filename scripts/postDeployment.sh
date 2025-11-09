a0f8d96e17c24d36c0d1a0304e2a0e55d5e4198c
Nathan Abondance <32196400+nabondance@users.noreply.github.com>
2025-11-09 17:37:28 +0100

    fix prepare pool

--- scripts/postDeployment.sh
+++ scripts/postDeployment.sh
@@ -1,2 +1,5 @@
-sf org assign permset --name EinsteinGPTPromptTemplateManager
-sf org assign permset --name AgentPlatformBuilder
+sf org list --json > orgs.json
+scratchorg=$(jq -r '.result.scratchOrgs[0].username' orgs.json)
+
+sf org assign permset --name EinsteinGPTPromptTemplateManager -o $scratchorg
+sf org assign permset --name AgentPlatformBuilder -o $scratchorg
